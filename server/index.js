4b49d33f55ad517ef4ca10a08fe0c3a980ecf34a
jyothiswaroopch <jchittiboyina@gmail.com>
2026-02-01 22:55:40 +0530

    Fix: use process.env.PORT for deployment

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const io = new Server(server, {
     }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 io.on('connection', (socket) => {
     console.log(`User connected: ${socket.id}`);
